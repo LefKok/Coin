03bef536c53628ec0de4e9ad4807281bb8df0017
Lefteris Kokoris Kogias <kokoris.kogias.eleftherios@gmail.com>
2015-10-27 16:45:06 +0100

    leader start

--- leader.go
+++ leader.go
@@ -1,65 +1,17 @@
 package main
 
 import (
-	//"encoding/hex"
-	"github.com/LefKok/Coin/blkparser"
+	"github.com/LefKok/Coin"
 	"log"
-	"os"
 )
 
 func main() {
 
-	f, err := os.OpenFile("Block_chain.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-
-	//log.SetOutput(f)
-
 	Magic := [4]byte{0xF9, 0xBE, 0xB4, 0xD9}
 
-	Chain, _ := blkparser.NewBlockchain("/home/lefteris/hi/blocks", Magic)
+	Parser, _ := Coin.NewParser("/home/lefteris/hi/blocks", Magic)
 
-	first_block := 0
-	last_block := 10000
-
-	var transactions []blkparser.Tx
-
-	for i := 0; i < last_block; i++ {
-		raw, err := Chain.FetchNextBlock()
-
-		if raw == nil || err != nil {
-			log.Println("End of Chain")
-		}
-
-		bl, err := blkparser.NewBlock(raw[:])
-
-		if err != nil {
-			println("Block inconsistent:", err.Error())
-			break
-		}
-
-		// Read block till we reach start_block
-		if i < first_block {
-			continue
-		}
-
-		//log.Printf("Current block height: %v", i)
-
-		// Basic block info
-		//log.Printf("Block hash: %v", bl.Hash)
-		//log.Printf("Block version: %v", bl.Version)
-		//log.Printf("Block parent: %v", bl.Parent)
-		//log.Printf("Block merkle root: %v", bl.MerkleRoot)
-		//log.Printf("Block size: %v", len(bl.Raw))
-
-		for _, tx := range bl.Txs {
-			transactions = append(transactions, *tx)
-		}
-
-		//log.Println()
-	}
+	transaction := Parser.parse(0, 10)
 
 	for _, tx := range transactions {
 
---
+++ parser.go
@@ -0,0 +1,52 @@
+package Coin
+
+import (
+	"github.com/LefKok/Coin/blkparser"
+	"log"
+)
+
+type Parser struct {
+	Path      string
+	Magic     [4]byte
+	CurrentId uint32
+}
+
+func NewParser(path string, magic [4]byte) (parser *Parser, err error) {
+	parser = new(Parser)
+	parser.Path = path
+	parser.Magic = magic
+	parser.CurrentId = 0
+}
+
+func (p *Parser) parse(first_block, last_block int) []blkparser.Tx {
+
+	Chain, _ := blkparser.NewBlockchain(p.Path, p.Magic)
+
+	var transactions []blkparser.Tx
+
+	for i := 0; i < last_block; i++ {
+		raw, err := Chain.FetchNextBlock()
+
+		if raw == nil || err != nil {
+			log.Println("End of Chain")
+		}
+
+		bl, err := blkparser.NewBlock(raw[:])
+
+		if err != nil {
+			println("Block inconsistent:", err.Error())
+			break
+		}
+
+		// Read block till we reach start_block
+		if i < first_block {
+			continue
+		}
+
+		for _, tx := range bl.Txs {
+			transactions = append(transactions, *tx)
+		}
+
+	}
+	return transactions
+}
